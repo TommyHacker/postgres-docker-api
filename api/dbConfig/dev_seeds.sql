8d49e3f19beb1e43f50ad4c8b660df0eb3da4947
TommyHacker <t.hacker.webdev@gmail.com>
2022-06-22 16:50:03 +0100

    fixed seeds

--- api/dbConfig/dev_seeds.sql
+++ api/dbConfig/dev_seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO users("username","password")
+INSERT INTO users(username,password)
 VALUES
-    ("tommy hacker", "password"),
-    ("reuben hacker", "password"),
-    ("simona hacker", "password"),
-    ("sarah west", "password");
+    ('tommy hacker', 'password'),
+    ('reuben hacker', 'password'),
+    ('simona hacker', 'password'),
+    ('sarah west', 'password');
